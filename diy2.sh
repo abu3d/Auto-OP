80e8eb72a48f676c24026b70f2d914decca142e7
abu3d <35095551+abu3d@users.noreply.github.com>
2022-01-17 01:05:05 +0800

    Update diy2.sh

--- diy2.sh
+++ diy2.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 # 修改默认IP
-sed -i 's/192.168.1.1/192.168.7.1/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.199.220/g' package/base-files/files/bin/config_generate
 # 修改默认主题
 sed -i 's/luci-theme-bootstrap/luci-theme-infinityfreedom/g' feeds/luci/collections/luci/Makefile
 #修改主机名
-sed -i 's/OpenWrt/Bin-Lean/g' package/base-files/files/bin/config_generate
+sed -i 's/OpenWrt/Abu3d-OP/g' package/base-files/files/bin/config_generate
 #关闭自建私有源签名验证
 sed -i '90d' package/system/opkg/Makefile
 #修改在线安装插件私有源
